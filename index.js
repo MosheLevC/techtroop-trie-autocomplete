30d04517af25b0e40120a7f0210da8331d8c51ee
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-16 19:19:34 +0300

    refactor: move app initialization to DOMContentLoaded event

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { createTrieNode } from "./trie.js";
-import { displayWelcome } from "./view.js";
 import { startApp } from "./controller.js";
 
 const root = createTrieNode();
 
-displayWelcome();
-startApp(root);
+document.addEventListener("DOMContentLoaded", () => {
+  startApp(root);
+});
